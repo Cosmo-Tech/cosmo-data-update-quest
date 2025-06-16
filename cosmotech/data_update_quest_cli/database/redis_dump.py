4a2785b9807dae8b05ea1f62dedcf053026c462a
Leopold Cramer <leopold.h.cramer@gmail.com>
2025-06-16 14:57:30 +0200

    remove types in redis_dump_command

--- cosmotech/data_update_quest_cli/database/redis_dump.py
+++ cosmotech/data_update_quest_cli/database/redis_dump.py
@@ -35,6 +35,6 @@ from cosmotech.data_update_quest_cli.utils.logger import LOGGER
 )
 @redis_connection_parameters
 @translate_help("data_update_quest.commands.redis_dump.description")
-def redis_dump_command(file_path: str, password: str, host: str, port: str, index_list: Optional[tuple]):
+def redis_dump_command(file_path, password, host, port, index_list: Optional[tuple]):
     redis_dump(file_path=file_path, host=host, port=port, password=password, index_list=index_list)
     LOGGER.info(T("data_update_quest.core.redis_dump.file_saved").format(file_path=file_path))
