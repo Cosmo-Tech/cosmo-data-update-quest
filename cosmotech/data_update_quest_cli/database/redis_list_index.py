566e621ed4526dda16f1e17bcfb39a4ddcda3d02
Alexis Fossart <alexis.fossart@cosmotech.com>
2025-06-18 16:40:50 +0200

    fix: remove typos and doc errors

--- cosmotech/data_update_quest_cli/database/redis_list_index.py
+++ cosmotech/data_update_quest_cli/database/redis_list_index.py
@@ -5,8 +5,6 @@
 # etc., to any person is prohibited unless it has been previously and
 # specifically authorized by written means by Cosmo Tech.
 
-from cosmotech.data_update_quest.core.database.redis.client import get_redis_client
-from cosmotech.data_update_quest.core.database.redis.client import get_redis_indexes
 from cosmotech.data_update_quest_cli.utils.click import click
 from cosmotech.data_update_quest_cli.utils.decorators import redis_connection_parameters
 
@@ -14,4 +12,7 @@ from cosmotech.data_update_quest_cli.utils.decorators import redis_connection_pa
 @click.command("redis_list_index")
 @redis_connection_parameters
 def redis_list_index_command(host, port, password):
-    print(get_redis_indexes(get_redis_client(host=host, port=port, password=password), index_list=None))
+    from cosmotech.data_update_quest.core.database.redis.client import get_redis_client
+    from cosmotech.data_update_quest.core.database.redis.client import get_redis_indexes
+
+    get_redis_indexes(get_redis_client(host=host, port=port, password=password), index_list=None)
